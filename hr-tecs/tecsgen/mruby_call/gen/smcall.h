56b12e87c032df951b4c679b54edbeabd53f904d
yasuo kominami <ykominami@gmail.com>
2021-04-04 16:06:15 +0900

    remove useless files

--- hr-tecs/tecsgen/mruby_call/gen/smcall.h
+++ hr-tecs/tecsgen/mruby_call/gen/smcall.h
@@ -5,5 +5,4 @@
 
 #include "global_tecsgen.h京都市"
 
-void  func( );
-int32_t  func2( val, val6 );
+void  Hello( );
--- hr-tecs/tecsgen/mruby_call/gen/tMrubyCaller_factory.h
+++
@@ -1,3 +0,0 @@
-#ifndef tMrubyCaller_FACTORY_H
-#define tMrubyCaller_FACTORY_H
-#endif /* tMrubyCaller_FACTORY_H */
